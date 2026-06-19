62ef60225da7f259685301d83e632099b1b4ec69
sureagar <asuresh.das@gmail.com>
2026-06-18 23:11:15 -0500

    Update Config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
-
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -12,59 +13,52 @@ const runId = new Date().toISOString().replace(/[:.]/g, '-');
 const runOutputDir = `test-results/pwreport-${runId}`;
 
 export default defineConfig({
-  timeout: 300000, // 5 minutes
+  timeout: 500000, // 5 minutes
   expect: {
     timeout: 10000, // 10 seconds
   },
   testDir: './e2e',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  // workers: 4,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: "playwright-report" }],
-  ['allure-playwright', { outputFolder: 'allure-results', detail: true, suiteTitle: true }],
-  ],
-  outputDir: `${runOutputDir}/artifacts`,
+  workers: process.env.CI ? 1 : 3,
+  
+    /* Reporter to use on CI. See https://playwright.dev/docs/test-reporters */
+  /* Reporter to use on CI. See https://playwright.dev/docs/test-reporters */
 
+  //  reporter:  [['html', {outputFolder: 'my-report'}], ['allure-playwright']],
+  reporter: [['html', { outputFolder: "playwright-report" }],
+['allure-playwright', { outputFolder: 'allure-results', detail: true, suiteTitle: true }],],
+outputDir: `${runOutputDir}/artifacts`,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: process.env.CI ? true : false,
-    // Base URL of the Application 
+    /* Base URL to use in actions like `await page.goto("/")`. */
     baseURL: 'https://vibetestq-osondemand.orangehrm.com/',
-    // baseURL: 'https://opensource-demo.orangehrmlive.com/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+
+    // trace: 'on',
+     trace: 'on-first-retry',
     actionTimeout: 10000,
     navigationTimeout: 60000,
-
+    headless: process.env.CI ? true : false,
   },
-
-  /* Configure projects for major browsers */
+  
+  /* Configure projects for major browsers. See https://playwright.dev/docs/api/class-testproject. */
   projects: [
     {
-      //https://playwright.dev/docs/test-projects#configure-projects-for-multiple-environments
-      name: 'vibetestq',
+       name: 'vibetestq',
       use: {
         baseURL: 'https://vibetestq-osondemand.orangehrm.com/',
         ...devices['Desktop Chrome'],
-
+        
       },
       // grep: "@ind", // to run tests with @ind tag
-      // testMatch: 'POMTestSerialMode.spec.ts'
-     // testMatch: 'POMWithDDT.spec.ts'
-    },
-    // {
-    //   // https://playwright.dev/docs/test-projects#configure-projects-for-multiple-environments
-    //   name: 'opensource',
-    //   use: {
-    //     baseURL: 'https://opensource-demo.orangehrmlive.com/',
-    //     ...devices['Desktop Chrome']
-    //   },
-    // }
-  ],
+      // testMatch: 'POMTestSerialMode.spec.ts', 
+      testMatch: ['*POMTestSerialMode.spec.ts', '*Apsrtc.spec.ts']
+      // testMatch:'CreateDelete.spec.ts'
+ } ],
 });
