834b7161ad9d0900f8613b3f0bf0f62f7f4d32eb
sureagar <asuresh.das@gmail.com>
2026-06-18 23:29:53 -0500

    Testmatch_POM

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,8 +57,8 @@ outputDir: `${runOutputDir}/artifacts`,
         
       },
       // grep: "@ind", // to run tests with @ind tag
-      // testMatch: 'POMTestSerialMode.spec.ts', 
-      testMatch: ['*POMTestSerialMode.spec.ts', '*Apsrtc.spec.ts']
+      testMatch: 'POMTestSerialMode.spec.ts', 
+     // testMatch: ['*POMTestSerialMode.spec.ts', '*Apsrtc.spec.ts']
       // testMatch:'CreateDelete.spec.ts'
  } ],
 });
